55db2b0356aa1633e444070cd9f1aee439f8ad63
Qais Nasir <qais@codek.tech>
2021-12-14 12:39:21 +0500

    done

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './Screens/AdminScreens/HomeScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hii</Text>
+      <HomeScreen/>
       <StatusBar style="auto" />
     </View>
   );

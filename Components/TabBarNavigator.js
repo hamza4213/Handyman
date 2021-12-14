d6af247d9acbbecd9c3525030b7318d552f0364b
IbtahajAli <88149767+IbtahajAli@users.noreply.github.com>
2021-12-14 01:14:55 -0800

    tab bar dn

---
+++ Components/TabBarNavigator.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Dimensions} from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import IonicIcon from 'react-native-vector-icons/Ionicons';
+import Home from '../Screens/AdminScreens/Home';
+import Profile from '../Screens/AdminScreens/Profile';
+
+const fullScreenWidth=Dimensions.get("window").width;
+const Tab=createBottomTabNavigator();
+
+const TabNavigator=()=>{
+    return (
+        
+        <Tab.Navigator
+            screenOptions={({route})=>({
+                tabBarStyle:{height:55,},
+                tabBarShowLabel:false,  
+                headerShown:false,
+                tabBarHideOnKeyboard:true,
+                // tabBarActiveBackgroundColor:"#3B9433",
+                // tabBarInactiveBackgroundColor:"#3B9433",
+                tabBarIcon: ({focused,color,size,padding,bottom,backgroundColor,borderRadius,width,paddingBottom,paddingLeft,borderBottomColor,borderBottomWidth})=>{
+                    let iconName;
+                    if(route.name === "Home"){
+                        iconName = focused ? 'home' : 'home-outline'
+                        size=28
+                    }
+                    else if(route.name === "Profile"){
+                        iconName = focused ? 'person' : 'person-outline'
+                        size=28
+                    }
+                   
+                    return(
+                        <IonicIcon
+                            name={iconName}
+                            size={size}
+                            color={color}
+                            style={{
+                                paddingBottom:padding,
+                                bottom:bottom, 
+                            }}
+
+                        />
+                    );
+                },
+            })}
+            tabBarOptions={{
+                activeTintColor:'#F1C40F',
+                inactiveTintColor:'#8c8c8c',
+                style:{width:fullScreenWidth,},
+            }}
+        >
+            <Tab.Screen name="Home" component={Home} />
+
+            <Tab.Screen name="Profile" component={Profile}/>
+
+        </Tab.Navigator>
+    );
+};
+
+export default TabNavigator;

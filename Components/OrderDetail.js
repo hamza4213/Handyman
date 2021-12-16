cd630e2580b9a65f85642327287b30696ba7278a
IbtahajAli <88149767+IbtahajAli@users.noreply.github.com>
2021-12-16 00:52:29 -0800

    order detail comp dn

--- Components/OrderDetail.js
+++ Components/OrderDetail.js
@@ -1,31 +1,43 @@
 import React from 'react';
 import { View, Text,TouchableOpacity} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 const OrderDetail = (props) => {
-    const{Text1,Text2,Text3,Text4,Text5,Text6,Text7,Text8,Text9}=props.item;
+    const {item} = props;
+    const navigation= useNavigation();
     return (
         <>
-        <TouchableOpacity style={{borderWidth:1,borderColor:'#8c8c8c',paddingLeft:'2%',padding:'5%',}}>
-            <Text style={{fontSize:18,fontWeight:'bold'}}>{Text1}</Text>
+        <TouchableOpacity 
+            style={{borderWidth:1,borderColor:'#8c8c8c',paddingLeft:'2%',padding:'4%',}}
+            onPress={()=>navigation.navigate("OrderHistoryScreen",{data:item})}
+        >
+            
+            <Text style={{fontSize:18,fontWeight:'bold'}}>{item.Text1}</Text>
+            
             <View style={{flexDirection:'row',}}>
-                <Text style={{fontSize:14,fontWeight:'bold'}}>{Text2}</Text>
-                <Text style={{fontSize:14,fontWeight:'bold'}}>{Text3}</Text>
+                <Text style={{fontSize:14,fontWeight:'bold'}}>{item.Text2}</Text>
+                <Text style={{fontSize:14,fontWeight:'bold'}}>{item.Text3}</Text>
             </View>
+            
             <View style={{flexDirection:'row',}}>
-                <Text style={{fontSize:14,fontWeight:'bold',color:'#8c8c8c'}}>{Text4}</Text>
-                <Text style={{fontSize:14,fontWeight:'bold',color:'#8c8c8c'}}>{Text5}</Text>
+                <Text style={{fontSize:14,fontWeight:'bold',color:'#8c8c8c'}}>{item.Text4}</Text>
+                <Text style={{fontSize:14,fontWeight:'bold',color:'#8c8c8c'}}>{item.Text5}</Text>
             </View>
+            
             <View style={{flexDirection:'row',}}>
-                <Text style={{fontSize:14,fontWeight:'bold',color:'#dcdcdc'}}>{Text6}</Text>
-                <Text style={{fontSize:14,fontWeight:'bold',color:'#dcdcdc'}}>{Text7}</Text>
+                <Text style={{fontSize:14,fontWeight:'bold',color:'#dcdcdc'}}>{item.Text6}</Text>
+                <Text style={{fontSize:14,fontWeight:'bold',color:'#dcdcdc'}}>{item.Text7}</Text>
             </View>
+            
             <View style={{flexDirection:'row',justifyContent:'space-between',marginRight:'12%'}}>
-                <Text style={{fontSize:14,fontWeight:'bold',color:'#dcdcdc'}}>{Text8}</Text>
-                <Text style={{fontSize:14,fontWeight:'bold',color:'#000'}}>{Text9}</Text>
+                <Text style={{fontSize:14,fontWeight:'bold',color:'#dcdcdc'}}>{item.Text8}</Text>
+                <Text style={{fontSize:14,fontWeight:'bold',color:'#000'}}>{item.Text9}</Text>
             </View>
             
         </TouchableOpacity>
-        <View style={{height:'0.5%'}}></View>
+        
+        <View style={{height:'0.05%'}}></View>
+        
         </>
     );
 };

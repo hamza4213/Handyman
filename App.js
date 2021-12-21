7342f0fec12d6b51da770e5ed413a88d347e56a0
Muhammadsheraz492 <kais.zwawi>
2021-12-21 17:27:22 +0500

    Cleaning the Code

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import UserSignIn from "./Screens/CustomerScreens/UserRegister";
 import Signin from "./Screens/CustomerScreens/Register";
 import Login from "./Screens/CustomerScreens/Login";
-import Check from "./Check";
+
 // Import the functions you need from the SDKs you need
 
 // Initialize Firebase
@@ -31,7 +31,6 @@ export default function App() {
           </UserStack.Group>
         ) : (
           <UserStack.Group>
-            {/* <UserStack.Screen name="Check" component={Check} /> */}
             <UserStack.Screen name="UserSignIn" component={UserSignIn} />
             <UserStack.Screen name="UserSignIn1" component={Signin} />
           </UserStack.Group>

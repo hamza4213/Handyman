65bf75f837ba23d66c40fd3a502c2ec54e2ec095
Muhammad Shahzaib Ishaq <shexbi119@gmail.com>
2021-12-22 08:04:52 +0500

    login propmpt successful

--- Components/Image.js
+++ Components/Image.js
@@ -1,15 +1,44 @@
 import { useNavigation } from "@react-navigation/native";
-import React from "react";
-import { View, Text, Image, TouchableOpacity } from "react-native";
+import React, { useState, useCallback, useEffect } from "react";
+import { View, Text, Image, TouchableOpacity, Alert } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Images = props => {
   const { item } = props;
   const navigation = useNavigation();
+  const [user, setUser] = useState({});
+
+  const getData = useCallback(
+    async () => {
+      try {
+        const value = await AsyncStorage.getItem("@user");
+        if (value !== null) {
+          setUser(JSON.parse(value));
+          // value previously stored
+        }
+      } catch (e) {
+        console.log(e);
+        // error reading value
+      }
+    },
+    [user]
+  );
+
+  useEffect(() => {
+    getData();
+
+    return () => {};
+  }, []);
   return (
     <View style={{ marginHorizontal: "17%", marginVertical: "5%" }}>
       <TouchableOpacity
-        onPress={() =>
-          navigation.navigate(`${item.navigateto}`, (header = item))}
+        onPress={() => {
+          user.id
+            ? navigation.navigate(`${item.navigateto}`, (header = item))
+            : Alert.alert("Login First", "Please Login to continue", [
+                { text: "OK", onPress: () => navigation.navigate("UserSignIn") }
+              ]);
+        }}
       >
         <Image
           source={item.IMAGE}

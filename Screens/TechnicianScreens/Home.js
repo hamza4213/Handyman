70cd59cb425e457bf65d7aab79c34548779f2854
hamza4213 <hamzashabbir4213@gmail.com>
2021-12-23 14:01:47 +0500

    Changings

--- Screens/TechnicianScreens/Home.js
+++ Screens/TechnicianScreens/Home.js
@@ -1,49 +1,115 @@
-import React from 'react';
-import { View, Text,Dimensions,FlatList,Image} from 'react-native';
-import Header3 from '../../Components/Header3';
-import IonicIcon from 'react-native-vector-icons/Ionicons';
+import React from "react";
+import { View, Text, Dimensions, FlatList, Image } from "react-native";
+import Header3 from "../../Components/Header3";
+import IonicIcon from "react-native-vector-icons/Ionicons";
 import Images3 from "../../Components/Images3";
 const Home = () => {
-    const Arr= [
-        {IMAGE:require('../../Images/img1.png'),height:50, width:50, Imgtext:'Order',navigateto:"OrderDetailScreen"},
-        {IMAGE:require('../../Images/img2.png'),height:50, width:50, Imgtext:'Aceept',navigateto:"AcceptOrderScreen"},
-        {IMAGE:require('../../Images/img3.png'),height:50, width:50, Imgtext:'Payment',navigateto:"PaymentScreen"},
-        {IMAGE:require('../../Images/more.png'),height:50, width:50, Imgtext:'more'},
-    ]
-    return (
-        <View style={{width:'100%',height:'100%',marginTop:'7%',backgroundColor:'#fff'}}>
-            
-            <View style={{width:'100%',height:'8%',}}>
-                <Header3 txt='HandyMan' txt2="SambalPur" Iconname1="menu" Iconname2="location-outline"  />
-            </View>
-            
-            <View style={{width:'100%',height:'4%',justifyContent:'center',flexDirection:'row',borderBottomWidth:1,borderColor:'#dcdcdc'}}>
-                <IonicIcon name='md-notifications-outline' color='#000' size={20}/>
-                <Text style={{fontSize:12,fontWeight:'bold',top:'0.5%',color:'#8c8c8c'}}>Please expect delays in services due to covid-19 restriction</Text>
-            </View>
+  const Arr = [
+    {
+      IMAGE: require("../../Images/img1.png"),
+      height: 50,
+      width: 50,
+      Imgtext: "Order",
+      navigateto: "OrderDetailScreen",
+    },
+    {
+      IMAGE: require("../../Images/img2.png"),
+      height: 50,
+      width: 50,
+      Imgtext: "Aceept",
+      navigateto: "AcceptOrderScreen",
+    },
+    {
+      IMAGE: require("../../Images/img3.png"),
+      height: 50,
+      width: 50,
+      Imgtext: "Payment",
+      navigateto: "PaymentScreen",
+    },
+    {
+      IMAGE: require("../../Images/more.png"),
+      height: 50,
+      width: 50,
+      Imgtext: "more",
+      navigateto: "PaymentScreen",
+    },
+  ];
+  return (
+    <View
+      style={{
+        width: "100%",
+        height: "100%",
+        marginTop: "7%",
+        backgroundColor: "#fff",
+      }}
+    >
+      <View style={{ width: "100%", height: "8%" }}>
+        <Header3
+          txt="HandyMan"
+          txt2="SambalPur"
+          Iconname1="menu"
+          Iconname2="location-outline"
+        />
+      </View>
 
-            <View style={{width:'100%',height:'5%',flexDirection:'row',alignItems:'center',paddingLeft:'3.5%',marginTop:'2%'}}>
-                <Text style={{fontWeight:'bold',fontSize:18}}>Most Used Services In ODISHA</Text>
-            </View>
+      <View
+        style={{
+          width: "100%",
+          height: "4%",
+          justifyContent: "center",
+          flexDirection: "row",
+          borderBottomWidth: 1,
+          borderColor: "#dcdcdc",
+        }}
+      >
+        <IonicIcon name="md-notifications-outline" color="#000" size={20} />
+        <Text
+          style={{
+            fontSize: 12,
+            fontWeight: "bold",
+            top: "0.5%",
+            color: "#8c8c8c",
+          }}
+        >
+          Please expect delays in services due to covid-19 restriction
+        </Text>
+      </View>
 
-            <View style={{width:'100%',height:'32%',}}>
-                <FlatList
-                    data={Arr}
-                    numColumns='2'
-                    keyExtractor={(item) => item.Imgtext}
-                    renderItem={({item}) => <Images3 item={item}/>}
-                />
-            </View>
+      <View
+        style={{
+          width: "100%",
+          height: "5%",
+          flexDirection: "row",
+          alignItems: "center",
+          paddingLeft: "3.5%",
+          marginTop: "2%",
+        }}
+      >
+        <Text style={{ fontWeight: "bold", fontSize: 18 }}>
+          Most Used Services In ODISHA
+        </Text>
+      </View>
 
-            <View style={{paddingLeft:'2%'}}>
-                <Text style={{fontSize:18}}>what do you need to find?</Text>
-            </View>
+      <View style={{ width: "100%", height: "32%" }}>
+        <FlatList
+          data={Arr}
+          numColumns="2"
+          keyExtractor={(item) => item.Imgtext}
+          renderItem={({ item }) => <Images3 item={item} />}
+        />
+      </View>
 
-            <View style={{height:'50%',}}>
-                <Image source={require('../../Images/taps.jpg')} style={{height:'100%'}}/>
-            </View>
+      <View style={{ paddingLeft: "2%" }}>
+        <Text style={{ fontSize: 18 }}>what do you need to find?</Text>
+      </View>
 
-        </View>
-    );
+      <View style={{ height: "50%" }}>
+        <Image
+          source={require("../../Images/taps.jpg")}
+          style={{ height: "100%" }}
+        />
+      </View>
+    </View>
+  );
 };
 export default Home;
--- Screens/TechnicianScreens/Profile.js
+++ Screens/TechnicianScreens/Profile.js
@@ -1,69 +1,128 @@
-import React from 'react';
-import { View, Text, FlatList,} from 'react-native';
-import ImagePick from '../../Components/ImagePick';
-import Images3 from '../../Components/Images3';
-import Header3 from '../../Components/Header3';
+import React from "react";
+import { View, Text, FlatList } from "react-native";
+import ImagePick from "../../Components/ImagePick";
+import Images3 from "../../Components/Images3";
+import Header3 from "../../Components/Header3";
 const Profile = () => {
-    const ARR=[ 
-        {   
-            Name:"RKDash",
-            Tel:"9556693650",
-            mail:"rptcwork@gmail.com"
-        } 
-    ]
-    const ARR1= [
-        {
-            IMAGE:require('../../Images/myOrder.png'),
-            height:50, width:50,borderRadius:20,backgroundColor:'#dcdcdc', Imgtext:'My Order',
-            navigateto:"OrderDetailScreen"
-        },
-        {
-            IMAGE:require('../../Images/adress.png'),
-            height:50,width:50,borderRadius:20, backgroundColor:'#dcdcdc', width:50, Imgtext:'Edit Adress'
-        },
-        {
-            IMAGE:require('../../Images/contact.png'),
-            height:50,width:50,borderRadius:20, backgroundColor:'#dcdcdc', width:50, Imgtext:'Contact US'
-        },
-        {
-            IMAGE:require('../../Images/lock.png'),
-            height:50, width:50, borderRadius:20,left:'-30%',left:'-32%',backgroundColor:'#dcdcdc', Imgtext:'Change Password'
-        },
-    ]
-    return (
-        <View style={{width:'100%',height:'100%',marginTop:'7%',alignItems:'center',backgroundColor:'#fff'}}>
-            <View style={{width:'100%',height:'10%',}}>
-                <Header3 txt='HandyMan' txt2="SambalPur" Iconname1="menu" Iconname2="location-outline"  />
-            </View>
-            <View style={{height:'40%',width:'100%',backgroundColor:'#FFD943',bottom:'4%',padding:'2%'}}>
-                <Text style={{fontSize:18,color:'#fff'}}>Profile</Text>
-                <View style={{height:'90%',width:'100%',justifyContent:'flex-end'}}>
-                    <ImagePick/>
-                </View>
-            </View>
-
-            <View style={{height:'10%',width:'100%',bottom:'3%'}}>
-                <FlatList
-                    data={ARR}
-                    keyExtractor={(item)=>item.mail}
-                    renderItem={({item})=>(
-                        <View style={{alignItems:'center',}}>
-                            <Text style={{fontWeight:'bold',fontSize:16}}>{item.Name}</Text>
-                            <Text style={{fontWeight:'bold',fontSize:16}}>{item.Tel}</Text>
-                            <Text style={{fontWeight:'bold',fontSize:16}}>{item.mail}</Text>
-                        </View>
-                    )}
-                />
-            </View>
-            <View style={{height:'30%',width:'100%',justifyContent:'center',bottom:'2%'}}>
-                <FlatList
-                    data={ARR1}
-                    numColumns='2'
-                    keyExtractor={(item) => item.Imgtext}
-                    renderItem={({item}) => <Images3 item={item} />}
-                />
-            </View>
+  const ARR = [
+    {
+      Name: "RKDash",
+      Tel: "9556693650",
+      mail: "rptcwork@gmail.com",
+    },
+  ];
+  const ARR1 = [
+    {
+      IMAGE: require("../../Images/myOrder.png"),
+      height: 50,
+      width: 50,
+      borderRadius: 20,
+      backgroundColor: "#dcdcdc",
+      Imgtext: "My Order",
+      navigateto: "OrderDetailScreen",
+    },
+    {
+      IMAGE: require("../../Images/adress.png"),
+      height: 50,
+      width: 50,
+      borderRadius: 20,
+      backgroundColor: "#dcdcdc",
+      width: 50,
+      Imgtext: "Edit Adress",
+      navigateto: "OrderDetailScreen",
+    },
+    {
+      IMAGE: require("../../Images/contact.png"),
+      height: 50,
+      width: 50,
+      borderRadius: 20,
+      backgroundColor: "#dcdcdc",
+      width: 50,
+      Imgtext: "Contact US",
+      navigateto: "OrderDetailScreen",
+    },
+    {
+      IMAGE: require("../../Images/lock.png"),
+      navigateto: "OrderDetailScreen",
+      height: 50,
+      width: 50,
+      borderRadius: 20,
+      left: "-30%",
+      left: "-32%",
+      backgroundColor: "#dcdcdc",
+      Imgtext: "Change Password",
+    },
+  ];
+  return (
+    <View
+      style={{
+        width: "100%",
+        height: "100%",
+        marginTop: "7%",
+        alignItems: "center",
+        backgroundColor: "#fff",
+      }}
+    >
+      <View style={{ width: "100%", height: "10%" }}>
+        <Header3
+          txt="HandyMan"
+          txt2="SambalPur"
+          Iconname1="menu"
+          Iconname2="location-outline"
+        />
+      </View>
+      <View
+        style={{
+          height: "40%",
+          width: "100%",
+          backgroundColor: "#FFD943",
+          bottom: "4%",
+          padding: "2%",
+        }}
+      >
+        <Text style={{ fontSize: 18, color: "#fff" }}>Profile</Text>
+        <View
+          style={{ height: "90%", width: "100%", justifyContent: "flex-end" }}
+        >
+          <ImagePick />
         </View>
-    );
+      </View>
+
+      <View style={{ height: "10%", width: "100%", bottom: "3%" }}>
+        <FlatList
+          data={ARR}
+          keyExtractor={(item) => item.mail}
+          renderItem={({ item }) => (
+            <View style={{ alignItems: "center" }}>
+              <Text style={{ fontWeight: "bold", fontSize: 16 }}>
+                {item.Name}
+              </Text>
+              <Text style={{ fontWeight: "bold", fontSize: 16 }}>
+                {item.Tel}
+              </Text>
+              <Text style={{ fontWeight: "bold", fontSize: 16 }}>
+                {item.mail}
+              </Text>
+            </View>
+          )}
+        />
+      </View>
+      <View
+        style={{
+          height: "30%",
+          width: "100%",
+          justifyContent: "center",
+          bottom: "2%",
+        }}
+      >
+        <FlatList
+          data={ARR1}
+          numColumns="2"
+          keyExtractor={(item) => item.Imgtext}
+          renderItem={({ item }) => <Images3 item={item} />}
+        />
+      </View>
+    </View>
+  );
 };
 export default Profile;
